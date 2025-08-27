1fa15a1584d9b3f69052b1b01fe50f5d5a2ae5be
ThoshithKautilya19 <143333995+ThoshithKautilya19@users.noreply.github.com>
2025-08-27 23:50:21 +0530

    classification on the sample dataset
    fitted and predicted, do the k-fold split

--- classification-exp.py
+++ classification-exp.py
@@ -14,5 +14,34 @@ X, y = make_classification(
 # For plotting
 plt.scatter(X[:, 0], X[:, 1], c=y)
 
+
+
 # Write the code for Q2 a) and b) below. Show your results.
 
+#convert to dataFrame
+X=pd.DataFrame(X,columns=["feat1", "feat2"])
+y=pd.Series(y)
+
+#train test splits
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+#fitting the tree for an exampl
+dec_tree=DecisionTree(criterion='entropy',max_depth=3)
+dec_tree.fit(X_train,y_train)
+
+#plotting the tree
+dec_tree.plot()
+
+#predicting for both info gain methods
+for criteria in ["information_gain", "gini_index"]:
+    tree = DecisionTree(criterion=criteria)
+    tree.fit(X_train, y_train)
+    y_hat = tree.predict(X_test)
+    tree.plot()
+    print("Criteria :", criteria)
+    print("Accuracy: ", accuracy(y_hat, y_test))
+    for cls in y_test.unique():
+        print("Precision: ", precision(y_hat, y_test, cls))
+        print("Recall: ", recall(y_hat, y_test, cls))   #classifiction task cant use RMSE, or MAE
+
+#results
