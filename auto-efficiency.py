3bf5eff585d218220bcee7798109bbec835efe0b
ThoshithKautilya19 <143333995+ThoshithKautilya19@users.noreply.github.com>
2025-08-27 23:12:33 +0530

    automotive efficiency cleaning

--- auto-efficiency.py
+++ auto-efficiency.py
@@ -13,4 +13,22 @@ data = pd.read_csv(url, delim_whitespace=True, header=None,
                         "acceleration", "model year", "origin", "car name"])
 
 # Clean the above data by removing redundant columns and rows with junk values
+# yes, a few columns that we will see onwards create a problem with improper values in the dataset
+
+#viewing the dataframe
+#data
+print(data.shape)
+print(data.isnull().sum()) # to see if any columns are not filled
+print(data.dtypes) # to see if any of the data types are not supported => float64s and int64s => handled well
+print(data.duplicated().sum()) #basic data pre-processing, check if any data is duplicated, if yes just remove it in the future. We didnt get any
+
+#So, while we were doing this we tried fitting and predicting the resulting tree
+#Importance of which metric to be used for what task is very important. For ex: dont use accuracy for a regression task
+
+
+
+
+
+
+
 # Compare the performance of your model with the decision tree module from scikit learn
